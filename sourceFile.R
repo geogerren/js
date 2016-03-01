e4c0c4de55bdb698c385e267fd64d8c9dd1ffcaa
DanJimu <dan.xu.chn@hotmail.com>
2016-03-01 14:32:46 +0800

    0301 commit

--- sourceFile.R
+++ sourceFile.R
@@ -1,2 +1,16 @@
 # source("~/jimu/sourceFile.R")
 box <- "E:/BaiduDropbox/jimu/"
+library(RMySQL)
+library(data.table)
+library(reshape2) # for melt and cast
+library(randomForest)
+# Jimu data queries
+drv <- dbDriver("MySQL")
+con <- dbConnect(drv, user="dumiao_analysis", password="analysis4321",
+              dbname="dumiao_analysis", host="172.19.1.221", port=9900, encoding = getOption("utf8"))
+jmq <- function(query) {
+  dbGetQuery(con, "SET NAMES 'GBK'")
+  resultDF <- dbGetQuery(con, query)
+  data.table(resultDF)
+}
+
