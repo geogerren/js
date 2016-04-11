bbfd9aa574e9ff5a4c29039a41d3af5a70352ad5
DanColumbia <dan.xu.chn@hotmail.com>
2016-04-11 08:31:55 +0800

    0411 commit

--- dumiaoA/dataProcessing/D1_moreVars.R
+++ dumiaoA/dataProcessing/D1_moreVars.R
@@ -1,5 +1,5 @@
 featuresWideU[, applyHour:=hour(createtime)]
-featuresWideU[, applyTimeSegment:=ifelse(applyHour>=1 & applyHour<=6, "UnexpectedTime", ifelse(applyHour>=9 & applyHour<=20, "WorkHour", "NonWorkHour"))]
+featuresWideU[, applyTimeSegment:=ifelse(applyHour>=1 & applyHour<=6, "3", ifelse(applyHour>=9 & applyHour<=20, "1", "2"))]
 
 
 
--- dumiaoA/dataProcessing/D2_naImpute.R
+++ dumiaoA/dataProcessing/D2_naImpute.R
@@ -1,10 +1,10 @@
 
 
 
-featuresWideU[financingprojectid==113187, card_tp:="platinum_card"]
-featuresWideU[card_tp=="3", card_tp:="golden_card"]
-featuresWideU[card_tp=="4", card_tp:="platinum_card"]
-featuresWideU[card_tp=="0", card_tp:="ordinary_card"]
+featuresWideU[financingprojectid %in% c(113187,120779), card_tp:="platinum_card"]
+featuresWideU[card_tp=="golden_card", card_tp:="3"]
+featuresWideU[card_tp=="platinum_card", card_tp:="4"]
+featuresWideU[card_tp=="ordinary_card", card_tp:="0"]
 
 
 ##############################################################################
@@ -32,10 +32,10 @@ featureAnalysis(featuresWideU, exclude=c("financingprojectid", "createtime", "ca
 
 
 typeConverter(featuresWideU, c("callBlacklist", "callLaws", "callNetLoanBlank",
-                               "cellphoneAuth", "card_tp", "ecpPhoneTag", "ecp_eachother", "hasShCISReport", 
-                               "inBlanklist", "highZhimaScore", "inJulixinBlanklist", "inZhimaBlank", "juxinliSuccess",
-                               "marry", "longTimeShutdown", "localFriends", "noNeedMobileAuthCheck", "sex", "normalContact", 
-                               "trustAddr", "trustIP", "flgDPD","FLAG_12_var1"), "factor")
+                                "card_tp", "ecpPhoneTag",  "hasShCISReport", 
+                                 "inJulixinBlanklist",  "juxinliSuccess",
+                               "marry", "longTimeShutdown", "localFriends",  "sex", "normalContact", 
+                                 "flgDPD","FLAG_12_var1"), "factor")
 
 
 typeConverter(featuresWideU, c("tachEcp"), "integer")
@@ -46,6 +46,9 @@ typeConverter(featuresWideU, c("tachEcp"), "integer")
 
 
 ###################################################################################
+featuresWideU[juxinliSuccess=='None', juxinliSuccess:="0"]
+featuresWideU[FLAG_12_var1=='None', FLAG_12_var1:="0"]
+
 trainData <- featuresWideU[is.na(flgValidation),]
 validateData<- featuresWideU[flgValidation==1,]
 
@@ -57,10 +60,12 @@ testImpute<-ggImpute(validateData, fullImpute = F, removeMassiveMissing = F)
 
 
 
+foreign::write.foreign(trainData, paste0(boxdata, "train.txt"),paste0(boxdata, "train.sas"),package = "SAS")
+foreign::write.foreign(trainData, paste0(boxdata, "test.txt"),paste0(boxdata, "test.sas"),package = "SAS")
 
 
-write.csv(trainData, paste0(boxdata, "train.csv"))
-
-write.csv(validateData, paste0(boxdata, "test.csv"))
-
+# write.csv(trainData, paste0(boxdata, "train.csv"))
+# 
+# write.csv(validateData, paste0(boxdata, "test.csv"))
+# 
 
